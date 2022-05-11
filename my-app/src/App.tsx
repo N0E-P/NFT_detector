614125bf053054f3400a77bd3ba7d24f58dec4ba
N0E-P <pecci.noe@gmail.com>
2022-05-11 22:45:51 +0200

    center 2

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Discord } from "./components/Discord"
 import { Metamask } from "./components/Metamask"
-import { Box } from "@material-ui/core"
+import { Box, Container } from "@material-ui/core"
 
 function App() {
   return (
     <div>
-      <Box>
+      <Container>
         <h1 >Welcome to the Discord NFT detector!</h1>
-      </Box>
+      </Container>
       <Box>
         <Metamask />
       </Box>
