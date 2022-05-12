5a6635ce87e8b869c29be236f52111de0720f43a
N0E-P <pecci.noe@gmail.com>
2022-05-12 11:31:49 +0200

    discord bot update

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -1,18 +1,31 @@
 import discord
 import os
+from dotenv import load_dotenv
 
+# Use the .env file
+load_dotenv()
+
+# Connect the discord bot
 client = discord.Client()
 
+# Connect the discord bot
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
 
+
+# Take the token to have authorization to act as the Discord bot
+client.run(os.getenv("TOKEN"))
+
+
+# Send the dapp link to the users when they write "$NFT"
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
-    
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+    # Make sure to not respond at his own messages
+    if message.author == client.user:
+        return
 
-client.run(os.getenv('TOKEN'))
+    # Send the Dapp Link
+    if message.content.startswith("$NFT"):
+        await message.channel.send("Here is the link to verify yout NFT:")
+        await message.channel.send("http://localhost:3000/")
