aadab53d175c5ab2cea98e22c8dd3a75d226bc0b
N0E-P <101148063+N0E-P@users.noreply.github.com>
2022-05-13 12:27:30 +0000

    beautiful messages

--- discord-bot/main.py
+++ discord-bot/main.py
@@ -13,7 +13,7 @@ client = discord.Client(intents=intents)
 # Connect the discord bot
 @client.event
 async def on_ready():
-    print("We have logged in as {0.user}".format(client))
+  print("We have logged in as {0.user}".format(client))
 
 
 # Send the dapp link to the users when they write "$NFT"
@@ -21,15 +21,28 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
-  msg = message.content
-  if msg.startswith("$NFT"):
-    await message.channel.send("Here is the link to verify your NFT: http://localhost:3000/")
 
+    
+  if message.content.startswith("$NFT"):
+    await message.channel.send("**Hey!** :sunglasses:") 
+    await message.channel.send("To initialize me, please write the command _$collection_, and paste the address of your _NFT collection_.")
+    await message.channel.send("```Example : $collection 0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB```")
+
+  
+  if message.content.startswith("$collection"):
+    enteredAddress = message.content.split("$collection ",1)[1]
+    #update_address(enteredAddress)
+    await message.channel.send("**Your collection was added successfuly!** :partying_face:")
+    await message.channel.send('You have only one thing to do:')
+    await message.channel.send('> Please create a role named "NFT member" to make NFT owners recognisable, and to let them access special channels.')
+    #await message.channel.send('Thank you for using NFT detector! :grin:')
+    
 
 # Send a DM with the Dapp link when someone join the server
 @client.event
 async def on_member_join(member):
-  await member.send(f'Hey {member.name}! If you want to verify your NFT to get a special role on your new discord server, click this link and follow the quick steps: http://localhost:3000/')
+  await member.send(f'**Hey {member.name}!** :grin:')
+  await member.send('If you want to verify your NFT to get a special role on your new discord server, click this link and follow the quick steps: http://localhost:3000/')
 
 
 #let the server online by using the keep_alive script
