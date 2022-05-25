8a5a59049f13fe834b0f2b7bf1f1861945487df5
N0E-P <101148063+N0E-P@users.noreply.github.com>
2022-05-25 07:49:48 +0000

    it finally restart to work like 2 hours ago

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const token = process.env['token']
 
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-// Before running the program : "npm install discord.js"
-// If "Error: Cannot find module ‘node:events’" : npm update -g
-
-//npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
-
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-
 
 const token = process.env['token']
 const CLIENT_ID = process.env['client_id']
 const GUILD_ID = process.env['guild_id']
-
 
 const commands = [{
   name: 'ping',
   description: 'Replies with Pong!'
 }]; 
 
-
 const rest = new REST({ version: '9' }).setToken(token);
-
 
 (async () => {
   try {
