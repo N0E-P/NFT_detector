7cc5ceefbffccf82fe89e0c58ae6ab1c2aabe178
N0E-P <101148063+N0E-P@users.noreply.github.com>
2022-05-25 05:53:28 +0000

    New bot in JS with test commands /ping but not up to date

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 // Before running the program : "npm install discord.js"
 // If "Error: Cannot find module ‘node:events’" : npm update -g
 
-const Discord = require("discord.js")
-const client = new Discord.Client()
+//npm init -y && npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH
 
-client.on("ready", () => {
-  console.log('Logged in as $ {client.user.tag}!')
-})
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
-client.on("message", msg => {
-  if (msg.content === 'ping') {
-    msg.reply("pong")
+
+const token = process.env['token']
+const CLIENT_ID = process.env['client_id']
+const GUILD_ID = process.env['guild_id']
+
+
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}]; 
+
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
   }
-})
-
-const mySecret = process.env['TOKEN']
-client.login(mySecret)
+})();
