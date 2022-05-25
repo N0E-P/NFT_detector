8ee7bfa955d04ff4911410f33bc5bd4f3ce477e1
N0E-P <101148063+N0E-P@users.noreply.github.com>
2022-05-25 17:28:54 +0000

    working version

--- bot.js
+++ bot.js
@@ -1,11 +1,24 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents} = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const token = process.env['token']
+const keepAlive = require("./server")
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
 
+client.on("guildMemberAdd", async (member) => {
+  msg.author.send('**Hey ${msg.author.username}!** :grin:')
+})
+
+
+client.on("message", (msg) => {
+  let isWelcomeMessage = msg.type === 'GUILD_MEMBER_JOIN'
+  if (isWelcomeMessage) {
+    msg.author.send('**Hey ${msg.author.username}!** :grin:')
+  }
+    
+})
+
+
+/*
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
@@ -14,4 +27,11 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+*/
+
+keepAlive()
 client.login(token);
