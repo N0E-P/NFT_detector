61cd198bf0b4fdcaf52c7a09bdda03ca82e24a41
N0E-P <101148063+N0E-P@users.noreply.github.com>
2022-05-27 00:22:46 +0000

    [Working] !NFT command with get collection address and cool messages

--- discord-bot/commands/nft.js
+++ discord-bot/commands/nft.js
@@ -10,8 +10,9 @@ module.exports = {
 
     // GET NFT COLLECTION ADDRESS
     message.reply('**Hey!** :sunglasses:')
-    channel.send('To initialize me, please send a message containing only the ETH address of your _NFT collection_.')
-    channel.send('```Example : 0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB```')
+    channel.send('To initialize me, please send a message containing only the ETH address of your NFT collection.')
+    channel.send('Example:')
+    channel.send('```0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB```')
                  
     const filter = (m) => {
       return m.author.id == message.author.id
@@ -33,18 +34,18 @@ module.exports = {
         return
       }
       
-      let text = 'Thanks! This answer has been collected:\n'
+      let text = "**Thanks! I've collected your answer :grin: :**  _"
       let address = message.content
 
       collected.forEach((message) => {
-        text += `${message.content}\n`,
+        text += `${message.content}_`,
         address = message.content
       })
       
       channel.send(text)
-      channel.send('You will need to wait a little bit until we find all the NFT owners of your collection.')
+      channel.send('I will send you a message when I have found all the NFT owners of your collection.')
+      channel.send('Just wait a little bit...')
       console.log('The address has been collected. Starting Moralis with getowners...')
-      //console.log(address)  //it should give the address of the collection
 
       
 
@@ -57,22 +58,18 @@ module.exports = {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /*
+/*
 
-    //During this time, please create a role named "NFT owner" to make them recognisable, and to let them access special channels.
-
-
-
-    // MORALIS PART (look in collection.tsx & getusernames for that)
-      //https://github.com/N0E-P/NFT_detector/blob/master/to-delete/dapp-pieces/Collection.tsx#L1
-      //https://github.com/N0E-P/NFT_detector/blob/master/to-delete/discord.py/getUsernames.js
+MORALIS PART (look in collection.tsx & getusernames for that)
+https://github.com/N0E-P/NFT_detector/blob/master/to-delete/dapp-pieces/Collection.tsx#L1
+https://github.com/N0E-P/NFT_detector/blob/master/to-delete/discord.py/getUsernames.js
 
 
-    // Save the address collection variable somewhere to be able to get it back later (link it to server, or server ID if I can get it. To then use the actual server ID in the repeted file to add the nft owners) 
+Save the address collection variable somewhere to be able to get it back later (link it to server, or server ID if I can get it. To then use the actual server ID in the repeted file to add the nft owners) 
 
 
-    //**Your collection was added successfuly!** :partying_face:
-    //Now, the last thing you need to do is to share the following link to the members of your server, so they can proove to the bot that they really own their NFTs by connecting their metamask
-    //LINK
-    //Thank you for using NFT detector !
-  */
+**Your collection was added successfuly!** :partying_face:
+Now, the last thing you need to do is to share the following link to the members of your server, so they can proove to the bot that they really own their NFTs by connecting their metamask
+LINK
+Thank you for using NFT detector !
+*/
