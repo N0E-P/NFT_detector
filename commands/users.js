6c4cf1817c93098ff9e035b06f0f35510b2a3f15
N0E-P <pecci.noe@gmail.com>
2022-06-16 17:43:08 +0200

    c'est pas ça, mais c'est très intéressant

--- commands/users.js
+++ commands/users.js
@@ -5,7 +5,54 @@ module.exports = {
     guildOnly: true,
     callback: ({ message, client }) => {
         console.log("testusers starting... ");
+        /*
+                //var user = allMembers.get("user.username");
+                //var userName = user.get("username");
+                var user = getValues(allMembers, "user")
 
+
+                
+
+                //var userName = await findNextMember(allMembers, currentNumber)
+
+                //console.log("Checking the member " + userName)
+                //await checkAMember(userName)
+
+
+
+        //return an array of values that match on a certain key
+        function getValues(obj, key) {
+            var objects = [];
+            for (var i in obj) {
+                if (!obj.hasOwnProperty(i)) continue;
+                if (typeof obj[i] == 'object') {
+                    objects = objects.concat(getValues(obj[i], key));
+                } else if (i == key) {
+                    objects.push(obj[i]);
+                }
+            }
+            return objects;
+        }
+
+
+
+'564395821236355072' => GuildMember { user: User { username: 'Chaussette',
+'691279414796877834' => GuildMember { user: User { username: 'Noé Test',
+'974205010504192032' => GuildMember { user: User { username: 'NFT detector',
+
+
+
+
+                var results = [];
+                var searchVal = "Chaussette";
+                for (var i = 0; i < allMembers.length; i++) {
+                    if (allMembers[i]["user"]["username"] == searchVal) {
+                        results.push(obj.list[i]);
+                    }
+                }
+
+
+                */
         ///////////////////////////////////// DELETE EVERYTHING ABOVE ///////////////////////////////////////////////////
 
         // add "client" in the callback params
@@ -30,18 +77,23 @@ module.exports = {
             while (currentNumber < memberNumber) {
                 currentNumber++;
 
-                var userName = allMembers.get("user");
-                console.log(userName)
 
-                //var userName = await findNextMember(allMembers, currentNumber)
+                var results = getCountryByCode(allMembers, 'Chaussette')
 
-                //console.log("Checking the member " + userName)
-                //await checkAMember(userName)
+
+                console.log(results)
             }
 
 
             console.log("The roles have been check for every members in the server");
             return
+        }
+
+
+        function getCountryByCode(allMembers, username) {
+            return allMembers.filter(
+                function (allMembers) { return allMembers.user.username == username }
+            );
         }
 
 
