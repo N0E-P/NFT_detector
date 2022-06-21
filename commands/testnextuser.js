89be94f5a0ac87839b2081beca6460737dca7e6f
N0E-P <pecci.noe@gmail.com>
2022-06-21 15:02:47 +0200

    testnextuser working. Need to join everything

--- commands/testnextuser.js
+++ commands/testnextuser.js
@@ -1,147 +1,30 @@
-/*
-//var user = allMembers.get("user.username");
-//var userName = user.get("username");
-var user = getValues(allMembers, "user")
-
-
-               
-//var userName = await findNextMember(allMembers, currentNumber)
-
-//console.log("Checking the member " + userName)
-//await checkAMember(userName)
-
-
-
-//return an array of values that match on a certain key
-function getValues(obj, key) {
-   var objects = [];
-   for (var i in obj) {
-       if (!obj.hasOwnProperty(i)) continue;
-       if (typeof obj[i] == 'object') {
-           objects = objects.concat(getValues(obj[i], key));
-       } else if (i == key) {
-           objects.push(obj[i]);
-       }
-   }
-   return objects;
-}
-
-
-
-'564395821236355072' => GuildMember { user: User { username: 'Chaussette',
-'691279414796877834' => GuildMember { user: User { username: 'Noé Test',
-'974205010504192032' => GuildMember { user: User { username: 'NFT detector',
-
-
-
-var results = [];
-var searchVal = "Chaussette";
-for (var i = 0; i < allMembers.length; i++) {
-   if (allMembers[i]["user"]["username"] == searchVal) {
-       results.push(obj.list[i]);
-   }
-}
-
-
-
-//MARCH PA
-function getCountryCode(results, username) {
-   return results.filter(
-       function (results) { return results["user"] }
-   );
-}
-
-
-
-//var allerr = results.user.username
-//var autre = getCountryCode(results, 'NFT detector')
-//var siteuple = results["user"]
-
-
-
-var autre = [];
-var searchVal = "Chaussette";
-for (var i = 0; i < results.length; i++) {
-    if (results[i]["user"]["username"] == searchVal) {
-        autre.push(results[i]);
-    }
-}
-
-
-
-// add "client" in the callback params
-
-
-        function testest(guild) {
-            return guild.members.filter(
-                function (guild) { return member => member.presence.status === "offline" }
-            );
-        }
-
-
-*/
 module.exports = {
     category: 'NFT Detector commands',
     description: 'initialise the bot',
-    permissions: ['ADMINISTRATOR'],
-    guildOnly: true,
-    callback: ({ message, client }) => {
-        console.log("testusers starting... ");
-
-        ///////////////////////////////////// DELETE EVERYTHING ABOVE ///////////////////////////////////////////////////
-
-
-        async function addRoles(client, message) {
+    callback: ({ message }) => {
+        console.log("testnextuser starting... ");
+        addRoles(message)
+        async function addRoles(message) {
+            const { guild } = message
+            /////////////// DELETE EVERYTHING ABOVE ///////////////
 
 
-            //get server members
-            const server = message.guild.id;
-            const data = client.guilds.cache.get(server)
-            const membersData = await data.members.fetch()
-
-            //get the number of members
-            const memberNumber = data.memberCount
-
-            //
-
-            const member = await findAMemberData(membersData, 'NFT detector')
-            console.log(member)
-            const test = member.GuildMember.name
-
-            console.log(test) //TO DELETE
-
-            //
-            /*
-            //loop to verify all the server members
-            var currentNumber = 0
-            while (currentNumber < memberNumber) {
-                currentNumber++;
-                //
+            //Get the list of all the members IDs
+            const allMembersData = await guild.members.fetch()
+            const allMembersID = allMembersData.map(member => member.id);
 
 
-                var results = getCountryByCode(membersData, 'NFT detector')
-
-                var userName = results //Use a for loop ? Or find another way 
+            for (var currentMemberNumber = 0; currentMemberNumber < guild.memberCount; currentMemberNumber++) {
 
 
-                //
-                //console.log(userName)
+                //Get all the infos of the current member
+                var memberInfos = guild.members.cache.get(allMembersID[currentMemberNumber])
+
+
+                /////////////// DELETE EVERYTHING UNDER ///////////////
+                console.log(memberInfos.user.username)
             }
-            */
-
-
-            console.log("The roles have been check for every members in the server");
-            return
+            console.log("End.");
         }
-
-
-        //Donne toutes les données d'un utilisateur précis quand on le recherche
-        function findAMemberData(allMembers, username) {
-            return allMembers.filter(
-                function (allMembers) { return allMembers.user.username == username }
-            );
-        }
-
-        addRoles(client, message) //start the script
     }
 }
