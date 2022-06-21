c5033a0439537f633c39696b098e5b0cc5b22ff1
N0E-P <pecci.noe@gmail.com>
2022-06-21 15:44:35 +0200

    had work, should work, but not anymore

--- commands/init.js
+++ commands/init.js
@@ -1,6 +1,5 @@
 // This file is used to run the command !init in discord, and to initialize the bot
-const DiscordJS = require('discord.js')
-const { Moralis, useMoralisWeb3Api, useMoralis } = require('moralis/node')
+const { Moralis } = require('moralis/node')
 
 
 //Specificities of the command
@@ -9,8 +8,11 @@ module.exports = {
   description: 'initialise the bot',
   permissions: ['ADMINISTRATOR'],
   guildOnly: true,
-  callback: ({ message, channel, client }) => {
+  callback: ({ message, channel }) => {
+
+    //Start the script
     console.log('INIT function starting...')
+    getCollectionAddress(message, channel);
 
 
     function getCollectionAddress(message, channel) {
@@ -58,10 +60,11 @@ module.exports = {
           mssg.react('🇵')
           mssg.react('🇦')
           mssg.react('🇫')
+          mssg.react('🇷')
 
 
           //Make sure that the bot only take the answer from the admin
-          const filter = (reaction, user) => {
+          const filter = (user) => {
             return user.id === message.author.id;
           };
 
@@ -72,7 +75,7 @@ module.exports = {
 
           // Save the chosen emoji into a variable
           let chosenEmoji
-          collector.on('collect', (reaction, user) => {
+          collector.on('collect', (reaction) => {
             chosenEmoji = reaction.emoji.name
           });
 
@@ -102,19 +105,17 @@ module.exports = {
             else if (chosenEmoji === '🇵') {
               blockchain = 'polygon'
             }
+            else if (chosenEmoji === '🇷') {
+              blockchain = 'rinkeby'
+            }
             else {
-              channel.send('⚠️  A wrong emoji has been choosen. Please write `!init` again to restart.  ⚠️')
+              message.reply('⚠️  A wrong emoji has been choosen. Please write `!init` again to restart.  ⚠️')
               return
             }
 
 
             //message
             console.log('The blockchain ' + blockchain + ' has been chosen.')
-
-
-            //Get the server ID
-            var server = message.guild.id;
-            console.log("The server ID is " + server)
 
 
             //Ending messages for the user
@@ -125,62 +126,144 @@ module.exports = {
 
 
             //Start the repeat function and chose the time to repeat in minutes
-            setInterval(addRoles, 1000 * 60 * 1, server, address, blockchain)
+            setInterval(addRoles, 60000 * 1, blockchain, address, message)
           })
         })
     }
 
 
-    async function addRoles(server, address, blockchain) {
-      console.log("addRoles function starting...");
-
-
+    async function addRoles(blockchain, address, message) {
       //Start Moralis
       const serverUrl = "https://zxhf5v44ppmy.usemoralis.com:2053/server";
       const appId = "FhT4qqcXkx6s4d6fBGWoLyEi10twqx3uarr8eLEP";
       Moralis.start({ serverUrl, appId });
 
 
-      //Get the owners with all the metadata
-      const options = { chain: blockchain, address: address };
-      let objectOwners = await Moralis.Web3API.token.getNFTOwners(options);
-      let stringOwners
-      let allOwners = ""
-      while (objectOwners.next) {
-        objectOwners = await objectOwners.next()
-        stringOwners = JSON.stringify(objectOwners)
-        allOwners = allOwners + stringOwners
+      //Get all server data
+      const { guild } = message
+
+
+      //Get the role ID and verify if the server has a role named NFT Owner
+      const role = guild.roles.cache.find((role) => { return role.name === "NFT Owner" })
+      if (!role) { return console.log('ERROR: There is no NFT Owner role on this server') }
+
+
+      //Get the owners list with all the metadata
+      const allOwners = await getAllOwners(blockchain, address);
+
+
+      //Get the list of all the users of the dapp
+      const allUsers = await Moralis.Cloud.run("getAllUsers");
+
+
+      //Get the list of all the members IDs
+      const allMembersData = await guild.members.fetch()
+      const allMembersID = allMembersData.map(member => member.id);
+
+
+      //loop to verify all the server members
+      for (var currentMemberNumber = 0; currentMemberNumber < guild.memberCount; currentMemberNumber++) {
+
+
+        //Get all the infos of the current member
+        var memberInfos = guild.members.cache.get(allMembersID[currentMemberNumber])
+
+
+        //Check this member
+        await checkAMember(allOwners, allUsers, role, memberInfos)
       }
 
 
-      // Save the data in the Moralis Database
-      const Address = Moralis.Object.extend("CollectionsAddresses");
-      const newAddress = new Address();
-      newAddress.set("Address", address);
-      newAddress.set("Blockchain", blockchain);
-      newAddress.set("Server", server);
-      newAddress.set("Data", allOwners);
-      await newAddress.save();
-      console.log("All the data has been saved in the Moralis Database.");
-
-
-      //Find if the owner address is a real owner
-      var ownerAddress = "0xA858DDc0445d8131daC4d1DE01f834ffcbA52Ef1" // TO MODIFY
-      var lowerOwnerAddress = ownerAddress.toLowerCase()
-      var lowerAllOwners = allOwners.toLowerCase()
-      var wordFound = lowerAllOwners.indexOf(lowerOwnerAddress);
-      if (wordFound > -1) {
-        console.log("The specific word exists");
-      } else {
-        console.log("The specific word doesn't exists");
-      }
-
-
-      //Ending message
-      console.log("addRoles function ended");
+      //End of the script
+      console.log("The roles have been check for every members in the server");
+      return
     }
 
 
-    getCollectionAddress(message, channel); //Start the script
+    async function getAllOwners(blockchain, address) {
+      var options = { chain: blockchain, address: address };
+      var objectAllOwners = await Moralis.Web3API.token.getNFTOwners(options);
+      var stringAllOwners = ""
+      while (objectAllOwners.next) {
+        objectAllOwners = await objectAllOwners.next()
+        stringAllOwners = stringAllOwners + JSON.stringify(objectAllOwners)
+      }
+      var allOwners = stringAllOwners.toLowerCase()
+      return allOwners
+    }
+
+
+    async function checkAMember(allOwners, allUsers, role, memberInfos) {
+      //Get the full memberName from memberinfos
+      const memberName = (memberInfos.user.username + "#" + memberInfos.user.discriminator)
+
+
+      // Check if the discord member has register on the database
+      const isOnDatabase = await isMemberNameOnDatabase(memberName, allUsers);
+
+
+      if (isOnDatabase == "yes") {
+        //The member register on the dapp.
+
+
+        //Get the address of the user
+        const userAddress = await Moralis.Cloud.run("getUserAddress", params = { memberName });
+
+
+        //Check if the user is an owner of the NFT
+        const isOnCollection = await isUserAddressInCollection(userAddress, allOwners);
+
+
+        if (isOnCollection == "yes") {
+          //The member register on the dapp and is an NFT owner. So he get the role
+          memberInfos.roles.add(role)
+          console.log(memberName + ' is an NFT Owner!');
+          return
+
+
+        } else {
+          //The member register on the dapp BUT isn't in the collection. So we remove the role if he have it
+          if (memberInfos.roles.cache.get(role.id)) {
+            memberInfos.roles.remove(role)
+          }
+          console.log(memberName + " register on the dapp BUT isn't in the collection.");
+          return
+        }
+
+
+      } else {
+        //The member didn't register on the app. So we remove the role if he have it
+        if (memberInfos.roles.cache.get(role.id)) {
+          memberInfos.roles.remove(role)
+        }
+        console.log(memberName + " didn't register on the dapp.");
+        return
+      }
+    }
+
+
+    async function isMemberNameOnDatabase(memberName, allUsers) {
+      //Search for the userName in the the list of all the users
+      //WARNING : Everything is put in lower case, in case of a user putt his username without the correct upper or lowercases. But it can cause problems if 2 users have the same username but with different cases.
+      const lowerMemberName = memberName.toLowerCase()
+      const wordFound = allUsers.indexOf(lowerMemberName);
+      if (wordFound > -1) {
+        return "yes"
+      } else {
+        return
+      }
+    }
+
+
+    async function isUserAddressInCollection(userAddress, allOwners) {
+      //Check if the user is in the metadata of the NFT collection by looking for his address
+      const lowerUserAddress = userAddress.toLowerCase()
+      const wordFound = allOwners.indexOf(lowerUserAddress);
+      if (wordFound > -1) {
+        return "yes"
+      } else {
+        return
+      }
+    }
   }
 }
