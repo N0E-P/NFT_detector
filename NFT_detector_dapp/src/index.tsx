e756cf3a27f29beda22ff81d9111f17f492c57d2
N0E-P <pecci.noe@gmail.com>
2022-06-27 16:23:59 +0200

    should work a peu pret

---
+++ NFT_detector_dapp/src/index.tsx
@@ -0,0 +1,20 @@
+// The ground of all the app
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { MoralisProvider } from "react-moralis";
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+
+root.render(
+  <React.StrictMode>
+    <MoralisProvider serverUrl="https://zxhf5v44ppmy.usemoralis.com:2053/server" appId="FhT4qqcXkx6s4d6fBGWoLyEi10twqx3uarr8eLEP">
+      <App />
+    </MoralisProvider>
+  </React.StrictMode>
+);
